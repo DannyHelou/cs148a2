7ba36daadda9617e7851f2f576b838c198df7880
Danny Helou <85280079+DannyHelou@users.noreply.github.com>
2024-04-07 13:29:29 -0400

    papers - variable adjustments made

--- papers.py
+++ papers.py
@@ -41,7 +41,7 @@ Recommended steps:
    private top-level functions, so you can choose not to implement these
    functions, and you can add others if you want to for your solution.
    For this task, we will be testing that you are building the correct tree,
-   not that you are doing it in a particular way. We will access your class
+   not that you are dateng it in a particular way. We will access your class
    in the same way as in the client code in the visualizer.
 
 3. Plan out what you'll need to do to implement the PaperTree initializer.
@@ -73,11 +73,11 @@ class PaperTree(TMTree):
     """A tree representation of Computer Science Education research paper data.
 
     === Private Attributes ===
-    _authors:
-        The authors of this paper in list
-    _doi:
-        the doi of this paper
-    These should store information about this paper's <authors> and <doi>.
+    _auths:
+        The auths of this paper in list
+    _date:
+        the date of this paper
+    These should store information about this paper's <auths> and <date>.
 
     === Inherited Attributes ===
     rect:
@@ -100,15 +100,14 @@ class PaperTree(TMTree):
     === Representation Invariants ===
     - All TMTree RIs are inherited.
     """
-    _authors: str
-    _doi: str
-    # TODO: Add the type contracts for your new attributes here
+    _auths: str
+    _date: str
 
-    def __init__(self, name: str, subtrees: List[TMTree], authors: str = '',
-                 doi: str = '', citations: int = 0, by_year: bool = True,
+    def __init__(self, name: str, subtrees: List[TMTree], auths: str = '',
+                 date: str = '', citations: int = 0, by_year: bool = True,
                  all_papers: bool = False) -> None:
-        self._doi = doi
-        self._authors = authors
+        self._date = date
+        self._auths = auths
         if all_papers:
             if by_year:
                 tree = _build_tree_from_dict(_load_papers_to_dict(True))
@@ -120,7 +119,7 @@ class PaperTree(TMTree):
             TMTree.__init__(self, name, subtrees, citations)
 
         """Initialize a new PaperTree with the given <name> and <subtrees>,
-        <authors> and <doi>, and with <citations> as the size of the data.
+        <auths> and <date>, and with <citations> as the size of the data.
 
         If <all_papers> is True, then this tree is to be the root of the paper
         tree. In that case, load data about papers from DATA_FILE to build the
@@ -161,16 +160,15 @@ def _get_data(num: int) -> Union[int, str]:
         return ''
 
 
-def _load_papers_to_dict_helper(dic: Dict, cat: List) -> None:
-    # print(cat)
-    if cat == []:
+def _lptd_helper(d: Dict, category: List) -> None:
+    if category == []:
         pass
-    top = cat[0]
-    if top not in dic:
-        dic[top] = {}
-    cat = cat[1:]
-    if len(cat) > 0:
-        _load_papers_to_dict_helper(dic[top], cat)
+    top = category[0]
+    if top not in d:
+        d[top] = {}
+    category = category[1:]
+    if len(category) > 0:
+        _lptd_helper(d[top], category)
 
 
 def _load_papers_to_dict(by_year: bool = True) -> Dict:
@@ -178,30 +176,29 @@ def _load_papers_to_dict(by_year: bool = True) -> Dict:
     If <by_year>, then use years as the roots of the subtrees of the root of
     the whole tree. Otherwise, ignore years and use categories only.
     """
-
     if not by_year:
-        dic = {}
-        cats = []
+        d = {}
+        categories = []
         with open(DATA_FILE, newline='') as csvfile:
             load_file = csv.reader(csvfile, delimiter=',')
             next(load_file)
             for row in load_file:
-                cats.append(row[3].split(': ') + [load_file.line_num])
-        for cat in cats:
-            _load_papers_to_dict_helper(dic, cat)
-        return dic
+                categories.append(row[3].split(': ') + [load_file.line_num])
+        for category in categories:
+            _lptd_helper(d, category)
+        return d
     else:
-        dic = {}
-        cats = []
+        d = {}
+        categories = []
         with open(DATA_FILE, newline='') as csvfile:
             load_file = csv.reader(csvfile, delimiter=',')
             next(load_file)
             for row in load_file:
-                cats.append([row[2]] + row[3].split(': ') + [
+                categories.append([row[2]] + row[3].split(': ') + [
                     load_file.line_num])
-        for cat in cats:
-            _load_papers_to_dict_helper(dic, cat)
-        return dic
+        for category in categories:
+            _lptd_helper(d, category)
+        return d
 
 
 def _build_tree_from_dict(nested_dict: Dict) -> List[PaperTree]:
